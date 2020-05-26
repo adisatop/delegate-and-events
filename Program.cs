35d39eb800db10109469cd0316769c3ba2f31a50
adisatop <adisailan2@gmail.com>
2020-05-26 13:10:11 +0300

    uploading file

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        public delegate void HandleSpeed (int speed) ;
+        
+
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.speedUp(100);
+        }
+
+
+        class Car {
+            HandleSpeed overSpeed;
+            HandleSpeed underLimitedSpeed;
+            public event HandleSpeed _wranningEvent;
+
+            int maxSpeed = 90;
+            int minSpeed = 50;
+
+            public Car() {
+                overSpeed = sendWarningEvent;
+                underLimitedSpeed = sendWarningEvent;
+            }
+            
+            public void speedUp(int currentSpeed) {
+                if (currentSpeed > maxSpeed) {
+                    _wranningEvent += overSpeed;
+                }
+                if (currentSpeed < minSpeed)
+                {
+                    _wranningEvent += drivingTooSlow;
+                }
+                if(currentSpeed>minSpeed && currentSpeed<maxSpeed){
+                    _wranningEvent += drivingInLimitedSpeed;
+                }
+                _wranningEvent.Invoke(currentSpeed);
+            }
+
+            public void sendWarningEvent(int speed) {
+                Console.WriteLine("You driving over the limited speed! ");
+            }
+
+            public void drivingInLimitedSpeed(int speed)
+            {
+                Console.WriteLine("You driving on limited speed! ");
+            }
+
+            public void drivingTooSlow(int speed)
+            {
+                Console.WriteLine("You driving too slow. speed up !");
+            }
+        }
+    }
+}
